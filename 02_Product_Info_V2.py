af71e7fb6d147743e190aa816c1857a9f110c6ed
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-10-21 12:39:22 +1300

    Print statement

---
+++ 02_Product_Info_V2.py
@@ -0,0 +1,9 @@
+
+print("Sea Salt Crackers:           Weight: 0.185 grams \n"
+"Griffins Snax:               Weight: 0.25  grams \n"
+"Pizza Shapes:                Weight: 0.19  grams \n"
+"Arnotts Cheds:               Weight: 0.25  grams\n"
+"Rosemary Wheat:              Weight: 0.17  grams\n"
+"Original Rice Crackers:      Weight: 0.1   grams\n")
+
+
