01343956e9db56815f2e9d96613c9aec6d105b31
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-10-26 12:16:26 +1300

    Done!

--- 02_Product_Info_V2.py
+++ 02_Product_Info_V2.py
@@ -8,12 +8,12 @@ product_6_name = input("What is the name of this product? ").title()
 
 
 # Weights of products
-product_1_weight_g = input("What is the weight of the {} in grams? ".format(product_1_name))
-product_2_weight_g = input("What is the weight of the {} in grams? ".format(product_2_name))
-product_3_weight_g = input("What is the weight of the {} in grams? ".format(product_3_name))
-product_4_weight_g = input("What is the weight of the {} in grams? ".format(product_4_name))
-product_5_weight_g = input("What is the weight of the {} in grams? ".format(product_5_name))
-product_6_weight_g = input("What is the weight of the {} in grams? ".format(product_6_name))
+product_1_weight_g = int(input("What is the weight of the {} in grams? ".format(product_1_name)))
+product_2_weight_g = int(input("What is the weight of the {} in grams? ".format(product_2_name)))
+product_3_weight_g = int(input("What is the weight of the {} in grams? ".format(product_3_name)))
+product_4_weight_g = int(input("What is the weight of the {} in grams? ".format(product_4_name)))
+product_5_weight_g = int(input("What is the weight of the {} in grams? ".format(product_5_name)))
+product_6_weight_g = int(input("What is the weight of the {} in grams? ".format(product_6_name)))
 
 
 product_1_weight_kg = product_1_weight_g / 1000
@@ -23,12 +23,12 @@ product_4_weight_kg = product_4_weight_g / 1000
 product_5_weight_kg = product_5_weight_g / 1000
 product_6_weight_kg = product_6_weight_g / 1000
 
-product_1_cost = input("What is the cost of {}? ".format(product_1_name))
-product_2_cost = input("What is the cost of {}? ".format(product_2_name))
-product_3_cost = input("What is the cost of {}? ".format(product_3_name))
-product_4_cost = input("What is the cost of {}? ".format(product_4_name))
-product_5_cost = input("What is the cost of {}? ".format(product_5_name))
-product_6_cost = input("What is the cost of {}? ".format(product_6_name))
+product_1_cost = input("What is the cost of {}? $".format(product_1_name))
+product_2_cost = input("What is the cost of {}? $".format(product_2_name))
+product_3_cost = input("What is the cost of {}? $".format(product_3_name))
+product_4_cost = input("What is the cost of {}? $".format(product_4_name))
+product_5_cost = input("What is the cost of {}? $".format(product_5_name))
+product_6_cost = input("What is the cost of {}? $".format(product_6_name))
 
 
 print()
