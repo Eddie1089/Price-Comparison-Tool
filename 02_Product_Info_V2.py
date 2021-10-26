067d5e9eaf21c7c291b6cffc8f252c1971b1f555
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-10-27 09:26:44 +1300

    Added comments, decimal places, and unit price

--- 02_Product_Info_V2.py
+++ 02_Product_Info_V2.py
@@ -7,7 +7,7 @@ product_5_name = input("What is the name of this product? ").title()
 product_6_name = input("What is the name of this product? ").title()
 
 
-# Weights of products
+# Weights of products in Grams
 product_1_weight_g = int(input("What is the weight of the {} in grams? ".format(product_1_name)))
 product_2_weight_g = int(input("What is the weight of the {} in grams? ".format(product_2_name)))
 product_3_weight_g = int(input("What is the weight of the {} in grams? ".format(product_3_name)))
@@ -15,7 +15,7 @@ product_4_weight_g = int(input("What is the weight of the {} in grams? ".format(
 product_5_weight_g = int(input("What is the weight of the {} in grams? ".format(product_5_name)))
 product_6_weight_g = int(input("What is the weight of the {} in grams? ".format(product_6_name)))
 
-
+# Product Weight in Kilograms
 product_1_weight_kg = product_1_weight_g / 1000
 product_2_weight_kg = product_2_weight_g / 1000
 product_3_weight_kg = product_3_weight_g / 1000
@@ -23,33 +23,41 @@ product_4_weight_kg = product_4_weight_g / 1000
 product_5_weight_kg = product_5_weight_g / 1000
 product_6_weight_kg = product_6_weight_g / 1000
 
-product_1_cost = input("What is the cost of {}? $".format(product_1_name))
-product_2_cost = input("What is the cost of {}? $".format(product_2_name))
-product_3_cost = input("What is the cost of {}? $".format(product_3_name))
-product_4_cost = input("What is the cost of {}? $".format(product_4_name))
-product_5_cost = input("What is the cost of {}? $".format(product_5_name))
-product_6_cost = input("What is the cost of {}? $".format(product_6_name))
+# Product Cost
+product_1_cost = float(input("What is the cost of {}? $".format(product_1_name)))
+product_2_cost = float(input("What is the cost of {}? $".format(product_2_name)))
+product_3_cost = float(input("What is the cost of {}? $".format(product_3_name)))
+product_4_cost = float(input("What is the cost of {}? $".format(product_4_name)))
+product_5_cost = float(input("What is the cost of {}? $".format(product_5_name)))
+product_6_cost = float(input("What is the cost of {}? $".format(product_6_name)))
 
+# Unit Price
+product_1_unit_price = product_1_cost / product_1_weight_kg
+product_2_unit_price = product_2_cost / product_2_weight_kg
+product_3_unit_price = product_3_cost / product_3_weight_kg
+product_4_unit_price = product_4_cost / product_4_weight_kg
+product_5_unit_price = product_5_cost / product_5_weight_kg
+product_6_unit_price = product_6_cost / product_6_weight_kg
 
 print()
 
-print("{}:  Weight: {} grams    Weight: {} kilograms    Cost: ${}".format(product_1_name, product_1_weight_g,
-                                                                          product_1_weight_kg, product_1_cost ))
+# Print statements
+print("{}:  Weight: {} grams    Weight: {:.3f} kilograms    Cost: ${:.2f}   Unit Price: ${:.3f}".format
+      (product_1_name, product_1_weight_g, product_1_weight_kg, product_1_cost, product_1_unit_price))
 
-print("{}:  Weight: {}  grams   Weight: {} kilograms    Cost: ${}".format(product_2_name, product_2_weight_g,
-                                                                          product_2_weight_kg, product_2_cost))
+print("{}:  Weight: {} grams    Weight: {:.3f} kilograms    Cost: ${:.2f}   Unit Price: ${:.3f}".format
+      (product_2_name, product_2_weight_g, product_2_weight_kg, product_2_cost, product_2_unit_price))
 
-print("{}:  Weight: {}  grams   Weight: {} kilograms    Cost: ${}".format(product_3_name, product_3_weight_g,
-                                                                          product_3_weight_kg, product_3_cost))
+print("{}:  Weight: {} grams    Weight: {:.3f} kilograms    Cost: ${:.2f}   Unit Price: ${:.3f}".format
+      (product_3_name, product_3_weight_g, product_3_weight_kg, product_3_cost, product_3_unit_price))
 
-print("{}:  Weight: {}  grams   Weight: {} kilograms    Cost: ${}".format(product_4_name, product_4_weight_g,
-                                                                          product_4_weight_kg, product_4_cost))
+print("{}:  Weight: {} grams    Weight: {:.3f} kilograms    Cost: ${:.2f}   Unit Price: ${:.3f}".format
+      (product_4_name, product_4_weight_g, product_4_weight_kg, product_4_cost, product_4_unit_price))
 
-print("{}:  Weight: {}  grams   Weight: {} kilograms    Cost: ${}".format(product_5_name, product_5_weight_g,
-                                                                          product_5_weight_kg, product_5_cost))
+print("{}:  Weight: {} grams    Weight: {:.3f} kilograms    Cost: ${:.2f}   Unit Price: ${:.3f}".format
+      (product_5_name, product_5_weight_g, product_5_weight_kg, product_5_cost, product_5_unit_price))
 
-print("{}:  Weight: {}  grams   Weight: {} kilograms    Cost: ${}".format(product_6_name, product_6_weight_g,
-                                                                          product_6_weight_kg, product_6_cost))
+print("{}:  Weight: {} grams    Weight: {:.3f} kilograms    Cost: ${:.2f}   Unit Price: ${:.3f}".format
+      (product_6_name, product_6_weight_g, product_6_weight_kg, product_6_cost, product_6_unit_price))
 
-
-
+print()
